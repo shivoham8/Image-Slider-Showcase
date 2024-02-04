d73a60c281f9d1ee661704a783187f0679569199
Ajay Gupta <ajaygupta230085@gmail.com>
2024-02-04 05:46:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener("click", function() {
+    let items = document.querySelectorAll(".item");
+    document.querySelector(".slider").appendChild(items[0]);
+} )
+
+prev.addEventListener("click", function() {
+    let items = document.querySelectorAll(".item");
+    document.querySelector(".slider").prepend(items[items.length - 1]);
+} )
+
+// setInterval(function() {
+//     let items = document.querySelectorAll(".item");
+//     document.querySelector(".slider").appendChild(items[0]);
+// }, 3500);
